07f6485f3baca4bea28059b4d864219477ac2545
OMGhow <fr0lenkov.aleksandr@yandex.ru>
2025-03-04 17:28:21 +0300

    add socket and env

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -1,4 +1,7 @@
 import { Scene } from "phaser";
+import { io } from "socket.io-client";
+
+const socket = io(import.meta.env.VITE_SOCKET_ADDRESS);
 
 export default class Boot extends Scene {
   constructor() {
@@ -10,6 +13,16 @@ export default class Boot extends Scene {
   }
 
   create() {
+    socket.emit("ping", "hello world");
+
+    socket.onAny((...args) => {
+      this.events.emit("onSocket", args);
+    });
+
+    this.events.on("emitSocket", (event: string, ...args: unknown[]) => {
+      socket.emit(event, ...args);
+    });
+
     this.scene.start("Preloader");
   }
 }
